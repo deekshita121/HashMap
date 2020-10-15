d10bfe35c08108a3e64c817d73426cd84ab863c2
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-15 16:17:04 +0530

    [deekshita] Add : Ability to print the frequency of words using linkedlist

---
+++ HashMap/src/main/java/com/capgemini/click/MyLinkedList.java
@@ -0,0 +1,63 @@
+package com.capgemini.click;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+public class MyLinkedList<K> {
+//	private static final Logger log = LogManager.getLogger(MyLinkedList.class);
+	public INode head;
+	public INode tail;
+
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void add(INode newNode) {
+		if (this.tail == null)
+			this.tail = newNode;
+		if (this.head == null)
+			this.head = newNode;
+		else {
+			INode tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	public void append(INode newNode) {
+		if (this.tail == null)
+			this.tail = newNode;
+		if (this.head == null)
+			this.head = newNode;
+		else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+
+	public INode pop() {
+		INode temp = this.head;
+		this.head = head.getNext();
+		return temp;
+	}
+
+	public INode search(K key) {
+		INode temp = head;
+		while (temp != null && temp.getNext() != null) {
+			if (temp.getKey().equals(key))
+				return temp;
+			else
+				temp = temp.getNext();
+		}
+		return null;
+	}
+
+	public void printMyNodes() {
+		System.out.println("My Nodes: " + head);
+	}
+
+	public String toString() {
+		return "MyLinkedListNodes{" + head + '}';
+	}
+}
